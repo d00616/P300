9759170fba8d3576d3c155344ef858b336f52237
Frank Holtz <frank-github2013@holtznet.de>
2013-10-12 15:38:31 +0200

    Register benennen

--- tools/modbusparser.php
+++ tools/modbusparser.php
@@ -1,8 +1,83 @@
 #!/usr/bin/php 
 <?php
-/*
-        reads p300 debug(1) output from stdin
-*/
+
+$txt=array();
+function restisterparser($address, $value, &$txt = false )
+{
+ if (!$txt) $txt=array();
+ 
+ if (($address>=0x400) && ($address!=0x413))
+ {
+         restisterparser( ($address-0x400)*2, $value >> 8, &$txt );
+         restisterparser( ($address-0x400)*2+1, $value & 0xff, &$txt );
+ }
+ 
+ switch ($address)
+ {
+         case 0:
+                 $txt[]="Stufe:".$value;
+                 break;
+         case 1:
+                 $txt[]="Volumenstrom:".$value;
+                 break;
+         case 2:
+                 $wd=array(
+                         1 => 'Mo',
+                         2 => 'Tu',
+                         4 => 'We',
+                         8 => 'Th',
+                        16 => 'Fr',
+                        32 => 'Sa',
+                        64 => 'Su'
+                         
+                 );
+                 $txt[]="Weekday:".$wd[$value];
+                 break;
+         case 3:
+                 $txt[]="Hour:".$value;
+                 break;
+         case 4:
+                 $txt[]="Minute:".$value;
+                 break;
+         case 7:
+                 if ($value==1) $txt[]="Kalibriert"; else $txt[]="Unkalibriert (".$value.")";
+                 break;
+         case 13:
+                 if ($value==4) $txt[]="Raumluftabh. Feuerstaette"; else 
+                 if ($value==0) $txt[]="Geschlossene Feuerstaette"; else
+                                $txt[]="?";
+                 break;
+         case 14:
+                 $txt[]="Bypass Max:".$value;
+                 break;
+         case 15:
+                 $txt[]="Bypass Min:".$value;
+                 break;
+         case 16:
+         case 17:
+         case 18:
+         case 19:
+                 $txt[]="T".($address-15).":".$value;
+                 break;
+         case 20:
+         case 21:
+                 $txt[]="S".($address-19).":".$value*2;
+                 break;
+         case 24:
+                 $txt[]="Heizregister:".$value;
+                 break;
+         case 0x413:
+                 $txt[]="Countdown:".$value;
+                 break;
+        default:
+                if ($address<1024)
+                {
+                        $txt[]=$address."=".$value;
+                }
+ }
+
+ if (count($txt)>0) return "(".implode(",",$txt).")";
+}
 
 $comm=array();
 
@@ -28,26 +103,29 @@ while ($line = preg_replace("/ +/"," ",fgets($stdin)))
                                 {
                                         if ( (isset($data[0])) && (isset($data[1])))
                                         {
+                                                // Startregister
+                                                $start = convertword($data[0],2);
+                                                // Anzahl der Register
+                                                $count = convertword($data[0],4);                                                
+  
                                                 // sende und empfangsdaten vorhanden
                                                 if (($data[0][1]==3) && ($data[1][1]==3))
                                                 {
                                                         echo "$name read ";
-                                                        $start = convertword($data[0], 2);
-                                                        $count = convertword($data[0],4);
                                                         for ($i=0;$i<$count;$i++)
                                                         {
-                                                                echo (int)($i+$start)."=".convertword($data[1],3+($i*2))." ";
+                                                                $val=convertword($data[1],3+($i*2));
+                                                                echo sprintf("%x=%x%s ",(int)($i+$start),$val,restisterparser($i+$start,$val));
                                                         }
                                                         echo "\n";
                                                 }
                                                 if (($data[0][1]==16) && ($data[1][1]==16))
                                                 {
                                                         echo "$name write ";
-                                                        $start = convertword($data[0], 2);
-                                                        $count = convertword($data[0],4);
                                                         for ($i=0;$i<$count;$i++)
                                                         {
-                                                                echo (int)($i+$start)."=".convertword($data[0],7+($i*2))." ";
+                                                                $val= convertword($data[0],7+($i*2));
+                                                                echo sprintf("%x=%x%s ",(int)($i+$start),$val,restisterparser($i+$start,$val));
                                                         }
                                                         echo "\n";
                                                 }
@@ -60,12 +138,13 @@ while ($line = preg_replace("/ +/"," ",fgets($stdin)))
                 } 
                 
         }
+                else echo $line;
 }
 fclose($stdin);
 //print_r($comm);
 
 function convertword($data, $address)
 {
-        if ( (isset($data[$address])) && (isset($data[$address+1]))) return ($data[$address]*256)+$data[$address+1];
+        if ( (isset($data[$address])) && (isset($data[$address+1]))) return (($data[$address]*256)+$data[$address+1]);
                 else return -1;
 }
